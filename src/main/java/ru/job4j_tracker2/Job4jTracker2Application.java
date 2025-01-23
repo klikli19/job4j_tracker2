1ac7c3cc868286d325cacae79f8d4d129dd8cca4
klikli19 <an.kilikova@yandex.ru>
2025-01-23 09:15:58 +0300

    Initial commit

---
+++ src/main/java/ru/job4j_tracker2/Job4jTracker2Application.java
@@ -0,0 +1,13 @@
+package ru.job4j_tracker2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jTracker2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jTracker2Application.class, args);
+    }
+
+}
